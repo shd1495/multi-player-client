52cb64995b33740ea1b7ab45366a7f63eede9570
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-31 00:07:21 +0900

    Initial commit

---
+++ Assets/Src/Codes/Handler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Src.Codes
+{
+    public class Handler
+    {
+        public static void InitialHandler(InitialResponse res)
+        {
+            try
+            {
+                GameManager.instance.GameStart();
+                GameManager.instance.player.UpdatePositionFromServer(res.x, res.y);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Error InitialHandler: {e.Message}");
+            }
+        }
+    }
+}
